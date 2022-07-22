902b6d16be69efd75855fbf0eb9cf8ad2f405976
Lionel Untereiner <lionel.untereiner@geosiris.com>
2022-07-22 17:35:44 +0200

    bunch of fixes

--- src/pyMBA.cpp
+++ src/pyMBA.cpp
@@ -132,10 +132,10 @@ struct python_mba {
 
         Eigen::Matrix3d rot_t = rot.transpose();
 
-        for(uint32 i = 0 ; i < vertex_count ; ++i)
+        for(uint32 i = 0 ; i < vertex_count_ ; ++i)
         {
             Eigen::RowVector3d rp = vertices_.row(i);
-            Eigen::Vector3d p = rp.tranpose();
+            Eigen::Vector3d p = rp.transpose();
             p = rot_t * p;
             Eigen::Vector4d temp = projectionTransformInv * p.homogeneous();
             p = temp.head<3>();
